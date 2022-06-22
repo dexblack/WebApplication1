59d2d11d9819576bbf89146b9085a254f5a221ea
dex black <3454616+dexblack@users.noreply.github.com>
2022-06-22 22:28:09 +1000

    Bound model to controllers and views. Modified Home page to display Skill Categories List.

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private WebApplication1Entities1 _entities = new WebApplication1Entities1();
         public ActionResult Index()
         {
-            return View();
+            return View(_entities.SkillCategories.ToList());
         }
 
         public ActionResult About()
---
+++ WebApplication1/Controllers/SkillCategoriesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class SkillCategoriesController : Controller
+    {
+        private WebApplication1Entities1 db = new WebApplication1Entities1();
+
+        // GET: SkillCategories
+        public ActionResult Index()
+        {
+            return View(db.SkillCategories.ToList());
+        }
+
+        // GET: SkillCategories/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SkillCategory skillCategory = db.SkillCategories.Find(id);
+            if (skillCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(skillCategory);
+        }
+
+        // GET: SkillCategories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SkillCategories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] SkillCategory skillCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SkillCategories.Add(skillCategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(skillCategory);
+        }
+
+        // GET: SkillCategories/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SkillCategory skillCategory = db.SkillCategories.Find(id);
+            if (skillCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(skillCategory);
+        }
+
+        // POST: SkillCategories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] SkillCategory skillCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(skillCategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(skillCategory);
+        }
+
+        // GET: SkillCategories/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SkillCategory skillCategory = db.SkillCategories.Find(id);
+            if (skillCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(skillCategory);
+        }
+
+        // POST: SkillCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            SkillCategory skillCategory = db.SkillCategories.Find(id);
+            db.SkillCategories.Remove(skillCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
